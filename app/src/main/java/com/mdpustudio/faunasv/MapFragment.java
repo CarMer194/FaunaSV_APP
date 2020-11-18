2c6c7707e8e81adddfa9a6a0127d96d7b9ca0e12
AlexanderRivz <cricririvera@gmail.com>
2020-11-17 23:23:30 -0600

    se agrego la funcionalidad de locacion por gps y se arreglo la camara de la locacion del mapa

--- app/src/main/java/com/mdpustudio/faunasv/MapFragment.java
+++ app/src/main/java/com/mdpustudio/faunasv/MapFragment.java
@@ -2,12 +2,21 @@ package com.mdpustudio.faunasv;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,7 +25,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.Objects;
+
 public class MapFragment extends Fragment {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    private boolean permissionDenied = false;
+
+    private GoogleMap map;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -29,13 +46,47 @@ public class MapFragment extends Fragment {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
+
+
+
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
+            map = googleMap;
+            map.setMinZoomPreference(8.0f);
+            map.setMaxZoomPreference(21.0f);
+
+
+            /*LatLng sydney = new LatLng(-34, 151);
             googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+            enableMyLocation();
         }
     };
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            if ( map != null) {
+                map.setMyLocationEnabled(true);
+                LocationManager lm = (LocationManager) requireActivity().getSystemService(Context.LOCATION_SERVICE);
+                assert lm != null;
+                @SuppressLint("MissingPermission") Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                assert location != null;
+                double longitude = location.getLongitude();
+                double latitude = location.getLatitude();
+                LatLng currentLocation = new LatLng(latitude, longitude);
+                map.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+
+
+
+            }
+        } else {
+            /* Permission to access the location is missing. Show rationale and request permission
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);*/
+            Toast.makeText(getActivity(), "nel prrito", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Nullable
     @Override
